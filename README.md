3674a8e6704f6cb2a3a98d1d2983ac80bbfd4760
Natalia Mellado <nataliamelladocani@gmail.com>
2024-02-17 00:10:58 -0300

    Update html, js, css and readme.

--- README.md
+++ README.md
@@ -1,52 +1,24 @@
 # Prueba - Fundamentos de programación en JavaScript
 
-En esta prueba validaremos nuestros conocimientos de Fundamentos de programación en JavaScript.
+![Desktop Screenshot](./assets/design/screenshot-desktop.png)
+![Desktop Mobile](./assets/design/screenshot-mobile.png)
 
-## Descripción
+## Construido con
+
+- [Midjourney](https://www.midjourney.com/jobs/a513b0c5-a57c-4173-9b5c-197aae411f9d)
+- [SuperHero API](https://www.superheroapi.com/)
+- [CanvasJS](https://canvasjs.com/)
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+- Método Mobile first 
+- Creatividad y cariño 💖
+
+## Desafío
 
 La revista semanal SuperHero Chile, se encuentra en plena fase de masificación mediante medios digitales, por lo tanto, se diseñó un plan para atraer más clientes y así aumentar el tráfico en su página web.
 En base a lo anterior, usted ha sido contratado para crear una aplicación dinámica que permita a los usuarios buscar un superhéroe y visualizar información referente al personaje buscado.
 
 Ahora bien, para obtener la información necesaria de los Superhéroes se deberá consultar a la API denominada “SuperHero API”, la cual, se encuentra disponible en https://www.superheroapi.com/. Esta API en particular requiere de una clave o access-token, que se entrega al iniciar sesión con una cuenta de Facebook. Recuerda leer la documentación disponible en la API y revisar el material llamado Apoyo Prueba - Encuentra tu SuperHero.
 
-Si tienes problemas para iniciar sesión con Facebook puedes utilizar esta clave: 4905856019427443
-
-## Requerimientos
-
-1. Crear la estructura básica para la página web implementando HTML, que incluya un formulario de búsqueda. Utilizar etiquetas semánticas de HTML5 para definir y separar las secciones. 
-
-2. Agregar estilos mediante frameworks o librería de CSS, implementando por ejemplo menús de navegación y botones. Los estilos los puedes agregar a tu gusto. Como se muestra en la siguiente imagen de referencia. 
-
-![](./assets/design/imagen1.png)
-
-3. Una vez ingresado el número del héroe a buscar y después de realizar un click sobre el botón de búsqueda, se debe capturar y validar la información para evitar búsquedas que contengan algún texto diferente a números y mostrar la información dinámicamente mediante la librería jQuery y CanvasJS con un gráfico de pastel. Para lograr todo esto se debe: 
-
-3.1 Capturar la información ingresada mediante eventos del DOM con jQuery. 
-
-3.2 Implementar funciones para separar la captura de la información ingresada por el usuario con la consulta a la API. 
-
-3.3 Comprobar la información ingresada por el usuario, la cual, solo debe ser un número. 
-
-3.4 Consultar la API mediante AJAX con la sintaxis de jQuery.
-
-3.5 Renderizar la información recibida por la API dinámicamente utilizando tarjetas (card) de Bootstrap. 
-
-3.6 Utilizar ciclos y métodos para arreglos u objetos que permitan recorrer, ordenar y mostrar la información. 
-
-3.7 Emplear la librería de gráficos CanvasJS, para mostrar dinámicamente información específica de cada superhéroe.
- 
-3.8 Implementar estructuras condicionales para generar alertas cuando existan errores en la búsqueda. 
-
-En la siguiente imagen, se puede observar el resultado de la búsqueda del id número: 213.
-
-![](./assets/design/imagen2.png)
-
-## Consideraciones y recomendaciones
-
-● Leer documentación oficial de [jQuery](https://jquery.com/).
-
-● Leer documentación de [Canvas JS](https://canvasjs.com/).
-
-● [SuperHero API](https://www.superheroapi.com/)
-
-● [Lista de Superhéroes](https://www.superheroapi.com/ids.html)
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,28 @@ $(document).ready(function() {
         event.preventDefault(); 
 
         showMain();
+        getCharacterData($('#numberInput').val());
+
+        const scrollMain = document.getElementById('main-container');
+        scrollMain.scrollIntoView({ behavior: 'smooth' });
     });
 
-    // Function to show the main container if the number is valid
-    function showMain() {
-        var numberInput = $('#numberInput').val();
-        var mainContainer = $('.main-container');
+    // Listener for the random button
+    $('#btn-random').click(function() {
+        let mainContainer = $('#main-container, #footer-container');
 
+        const randomId = Math.floor(Math.random() * 732) + 1;
+        getCharacterData(parseInt(randomId));
+        mainContainer.css('display', 'block');
+
+        const scrollMain = document.getElementById('main-container');
+        scrollMain.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    // Function to show the main and footer if the number is valid
+    function showMain() {
+        let numberInput = $('#numberInput').val(),
+            mainContainer = $('#main-container, #footer-container');
 
         if (validateNumber(numberInput)) {
             mainContainer.css('display', 'block');
@@ -21,4 +36,92 @@ $(document).ready(function() {
         var parsedNumber = parseInt(number, 10);
         return !isNaN(parsedNumber) && parsedNumber >= 1 && parsedNumber <= 732;
     }
+
+    // Use of ajax to get the data from the API
+    function getCharacterData(number) {
+        $.ajax({
+            url: `https://superheroapi.com/api.php/2619421814940190/${number}`,
+            method: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                if (data.response === 'success') {
+                    
+                    const bioContainer = $('.bio');
+                    const statsContainer = $('#chartContainer');
+                    
+                    // Template for the bio
+                    const bioTemplate = `
+                        <article class="bio-content">
+                            <h3 class="bio-content__name">${data.name}</h2>
+                            <ul class="bio-content__des">
+                                <li>
+                                    <strong>Conexiones: </strong>${data.connections["group-affiliation"]}
+                                </li>
+                                <li>
+                                    <strong>Publicado por: </strong>${data.biography.publisher}
+                                </li>
+                                <li>
+                                    <strong>Ocupación: </strong>${data.work.occupation}
+                                </li>
+                                <li>
+                                    <strong>Primera Aparición: </strong>${data.biography["first-appearance"]}
+                                </li>
+                                <li>
+                                    <strong>Altura: </strong>${data.appearance.height.join(" - ")}
+                                </li>
+                                <li>
+                                    <strong>Peso: </strong>${data.appearance.weight.join(" - ")}
+                                </li>
+                                <li>
+                                    <strong>Aliases: </strong>${data.biography.aliases}
+                                </li>
+                            </ul>
+                        </article>
+                        <article class="bio-img">
+                            <img src="${data.image.url}" alt="Hero picture" />
+                        </article>
+                    `;
+                    bioContainer.empty();
+                    bioContainer.append(bioTemplate);
+
+                    // Template for the stats
+                    const datosXY = [];
+                    for (let key in data.powerstats) {
+                        datosXY.push({ 
+                            label: key,
+                            y: parseInt(data.powerstats[key])
+                        });
+                    }
+
+                    const statTemplate = {
+                        title: {
+                            text: `ESTADÍSTICAS DE PODER`,
+                            fontFamily: "Work Sans",
+                            fontSize: "30",
+                            fontWeight: "700",
+                            fontColor: "rgb(58, 31, 151)",
+                            margin: 40,
+                        },
+
+                        data: [
+                            {
+                              type: "pie",
+                              startAngle: 45,
+                              showInLegend: "true",
+                              legendText: "{label}",
+                              indexLabel: "{label} ({y})",
+                              yValueFormatString: "#,##0.#" % "",
+                              dataPoints: datosXY,
+                            },
+                          ],
+                    };
+
+                    statsContainer.empty();
+                    statsContainer.CanvasJSChart(statTemplate);
+
+                }
+            }
+        });
+    }
+
 });
